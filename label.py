87fb739498b8078fd0c57a7f6267f94b4e07de6a
HouBinyang <21302010042@m.fudan.edu.cn>
2024-05-16 03:14:21 +0800

    docs: add file and comments

--- label.py
+++ label.py
@@ -6,10 +6,21 @@ from scipy.ndimage.filters import gaussian_filter
 
 
 def parse_xml(xml_file, image_shape):
+    """
+    解析XML文件，生成一个与图像形状相同的密度图
+    """
+
+    # 解析xml文件
     tree = ET.parse(xml_file)
     root = tree.getroot()
+
+    # 初始化密度图
     density_map = np.zeros(image_shape, dtype=np.float32)
+
+    # 遍历xml文件中的所有 <object>
     for obj in root.findall('object'):
+
+        # 获取目标的坐标
         try:
             x = int(obj.find('point/x').text)
             y = int(obj.find('point/y').text)
@@ -20,19 +31,29 @@ def parse_xml(xml_file, image_shape):
             ymax = int(obj.find('bndbox/ymax').text)
             x = (xmin + xmax) // 2
             y = (ymin + ymax) // 2
+
+        # 将目标的坐标转换为密度图上的坐标
         if 0 <= y < image_shape[0] and 0 <= x < image_shape[1]:
             density_map[y, x] = 1
+
+    # 使用高斯滤波平滑密度图
+    # https://baike.baidu.com/item/%E9%AB%98%E6%96%AF%E6%BB%A4%E6%B3%A2/9032353
     density_map = gaussian_filter(density_map, sigma=15)
     return density_map
 
 
 xml_path = "./dataset/train/labels/"
 mat_path = "./dataset/train/hdf5s/"
+
+# 宽640，高512
 image_shape = (512, 640)  # Assuming the image shape is fixed
 
 for xml_file in os.listdir(xml_path):
     if xml_file.endswith(".xml"):
         print(xml_file)
         density_map = parse_xml(os.path.join(xml_path, xml_file), image_shape)
+
+        # hdf view
+        # https://www.hdfgroup.org/downloads/hdfview/#download
         with h5py.File(os.path.join(mat_path, xml_file.replace("R.xml", ".h5")), 'w') as hf:
             hf['density'] = density_map
--- model.py
+++ model.py
@@ -2,18 +2,32 @@ import torch.nn as nn
 import torch
 from torchvision import models
 
+# CSRNet 论文
+# https://arxiv.org/abs/1802.10062
+
 
 class CSRNet(nn.Module):
     def __init__(self, load_weights=False):
         super(CSRNet, self).__init__()
         self.seen = 0
+
+        # 前端配置，for 2D feature extraction
+        # 10个卷积层（VGG16的前10层），3个最大池化层。
         self.frontend_feat = [64, 64, 'M', 128, 128,
                               'M', 256, 256, 256, 'M', 512, 512, 512]
+
+        # 后端配置，to deliver larger reception fields and  replace pooling operations
+        # 使用 Dilated convolution （扩展的卷积层）
         self.backend_feat = [512, 512, 512, 256, 128, 64]
+
+        # 前端和后端
         self.frontend = make_layers(self.frontend_feat)
         self.backend = make_layers(
             self.backend_feat, in_channels=512, dilation=True)
+
+        # 输出层
         self.output_layer = nn.Conv2d(64, 1, kernel_size=1)
+
         if not load_weights:
             mod = models.vgg16(pretrained=True)
             self._initialize_weights()
@@ -27,6 +41,7 @@ class CSRNet(nn.Module):
         x = self.output_layer(x)
         return x
 
+    # 初始化权重
     def _initialize_weights(self):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -39,6 +54,12 @@ class CSRNet(nn.Module):
 
 
 def make_layers(cfg, in_channels=3, batch_norm=False, dilation=False):
+    """
+    cfg: list, 卷积层配置，这里分别接收前后端的配置
+    in_channels: 输入通道数
+    batch_norm: 是否使用BN
+    dilation: 是否使用扩展卷积
+    """
     if dilation:
         d_rate = 2
     else:
