780e1504fdd4d8990e7abb7d190b2ef63ca408ad
polaris <2673986789@qq.com>
2024-05-19 20:09:27 +0800

    chore: add annotation

--- expand.py
+++ expand.py
@@ -15,7 +15,9 @@ if __name__ == "__main__":
     output_tir_dir = f"{output_dir}/tir"
     output_labels_dir = f"{output_dir}/labels"
     # split_utils.rm_dir(output_dir)
+    os.makedirs(output_dir, exist_ok=True)
     shutil.rmtree(output_dir, ignore_errors=False)
+    print("Copying dataset...")
     shutil.copytree("dataset/train", output_dir)
     print("Copying done")
 
@@ -24,10 +26,12 @@ if __name__ == "__main__":
     os.makedirs(output_labels_dir, exist_ok=True)
 
     if flip:
+        print("Flipping images...")
         flip_image.flip(output_rgb_dir, output_tir_dir, output_labels_dir)
         print("Flipping done")
 
     if downsample:
+        print("Downsampling images...")
         downsample_combine.downsample_combine(
             output_rgb_dir, output_tir_dir, output_labels_dir
         )
@@ -38,13 +42,16 @@ if __name__ == "__main__":
         temp_rgb_dir = f"{temp_dir}/rgb"
         temp_tir_dir = f"{temp_dir}/tir"
         temp_labels_dir = f"{temp_dir}/labels"
+        os.makedirs(temp_dir, exist_ok=True)
         split_utils.rm_dir(temp_dir)
 
         os.makedirs(temp_rgb_dir, exist_ok=True)
         os.makedirs(temp_tir_dir, exist_ok=True)
         os.makedirs(temp_labels_dir, exist_ok=True)
+        print("Splitting images...")
         split.split(temp_rgb_dir, temp_tir_dir, temp_labels_dir).split()
         print("Splitting done")
+        print("Combining images...")
         combine.combine(
             temp_rgb_dir,
             temp_tir_dir,
@@ -55,4 +62,6 @@ if __name__ == "__main__":
         ).combine()
         print("Combining done")
 
+    print("Labeling images...")
     label.label(output_labels_dir, output_labels_dir.replace("labels", "hdf5s"))
+    print("Labeling done")
